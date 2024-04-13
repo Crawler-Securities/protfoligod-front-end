7867762261c8b603ba7b6f791c6a82738da3f55d
hony <hony2323@gmail.com>
2024-04-13 11:07:49 +0300

    doesn't work with popup disallowed

--- src/components/CPAPIAuthPopup/index.tsx
+++ src/components/CPAPIAuthPopup/index.tsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import {IBKRHandler} from "../../services/backend/IBKR/IBKRHandler";
+import {Alert, Snackbar} from "@mui/material";
 
 const CPAPIAuthPopup: React.FC = () => {
     const [popup, setPopup] = useState<Window | null>(null);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     // URL for the CPAPI authentication page
     const authUrl = "https://localhost:5000/";
@@ -36,32 +38,37 @@ const CPAPIAuthPopup: React.FC = () => {
 
     return (
         <div>
-            <button onClick={() => {
+            <button onClick={async () => {
+                let ibkrh = new IBKRHandler();
+                let res = await ibkrh.isConnectedStatus();
+                if (res) {
+                    setSnackbarOpen(true);
+                    return;
+                }
+                debugger
                 openPopup();
                 checkPopupClosed();
             }}>Authenticate with IBKR
             </button>
-            <button onClick={() => {
-                let ibkrh = new IBKRHandler();
-                let res = ibkrh.isConnectedStatus().catch(error => {
-                    if (error.response) {
-                        // The request was made and the server responded with a status code
-                        // that falls out of the range of 2xx
-                        console.error('Error response:', error.response.data);
-                        console.error('Error status:', error.response.status);
-                        console.error('Error headers:', error.response.headers);
-                    } else if (error.request) {
-                        // The request was made but no response was received
-                        console.error('Error request:', error.request);
-                    } else {
-                        // Something happened in setting up the request that triggered an Error
-                        console.error('Error message:', error.message);
-                    }
-                });
-                ;
-            }}>Check Connection Status
-            </button>
+
+            <Snackbar
+                anchorOrigin={{vertical: 'bottom', horizontal: 'right'}}
+                open={snackbarOpen}
+                autoHideDuration={5000}
+                onClose={() => setSnackbarOpen(false)}
+                message="IBKR is already connected">
+                <Alert
+                    onClose={() => setSnackbarOpen(false)}
+                    severity="info"
+                    variant="filled"
+                    sx={{width: '100%'}}
+                >
+                    IBKR is already connected
+                </Alert>
+            </Snackbar>
         </div>
+
+
     );
 }
 
--- src/services/backend/IBKR/IBKRHandler.ts
+++ src/services/backend/IBKR/IBKRHandler.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
-import { IBKRHandlerInterface } from "./IBKRHandlerInterface";
+import {IBKRHandlerInterface} from "./IBKRHandlerInterface";
 
 export {}
+
 export class IBKRHandler implements IBKRHandlerInterface {
     async getAccountSyncData() {
         throw new Error('Method not implemented.');
     }
-    async isConnectedStatus() {
-        let res = await axios.get('http://localhost:8000/ibkr/connection-status');
-        debugger
-        return res.data;
+
+    async isConnectedStatus(): Promise<boolean> {
+        try {
+            const response = await axios.get('http://localhost:8000/ibkr/connection-status');
+            return response.data.connected;
+        } catch (error: unknown) {
+            console.error("Error fetching connection status:", error);
+            throw error; // Consider any other error as not connected
+        }
     }
 }
