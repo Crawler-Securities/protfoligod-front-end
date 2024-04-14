2a2232a39e6f36aaec5aa7814055d7ed3e812889
hony <hony2323@gmail.com>
2024-04-14 14:21:12 +0300

    Just a lil renaming

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,3 @@
-import React, {useEffect, useState} from 'react';
-import './styles.css';
-import PortfolioThumbnail from '../../components/PortfolioThumbnail';
-import {fetchData} from "../../services/backend";
-import useConfig from "../../utils/hooks/useConfig";
-import CPAPIAuthPopup from "../../components/CPAPIAuthPopup";
-
-const Home: React.FC = () => {
-
-    const [data, setData] = useState(undefined as any);
-
-    useEffect(() => {
-        const getData = async () => {
-            const result = await fetchData();
-            setData(result);
-        };
-
-        getData();
-    }, []);
-    const config = useConfig();
-    const CPAPIUrl = config.CPAPIUrl;
-    const authUrl = `${CPAPIUrl}`;
-
-    return (
-        <div className="home">
-            <div className="grid-container">
-                <div>{config ? `API Base URL: ${config.apiBaseUrl}` : 'Loading config...'}</div>
-                {/*<PortfolioThumbnail/>*/}
-                <CPAPIAuthPopup authUrl={authUrl}/>
-                {/* More thumbnails or components can be added here */}
-            </div>
-        </div>
-    );
-}
+import Home from "./Home";
 
 export default Home;
-
